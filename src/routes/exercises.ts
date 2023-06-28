0e5c171bf46fa046ef444caf57d5282ea70b383a
Lucas Cristovam <cristhovamlucas@gmail.com>
2023-06-28 18:00:58 -0300

    FEAT: new route for creating a exercise

--- src/routes/exercises.ts
+++ src/routes/exercises.ts
@@ -4,10 +4,19 @@ import { z } from 'zod';
 import { authenticate } from '../plugins/authenticate';
 
 export async function exercisesRoutes(fastify: FastifyInstance) {
-     fastify.get('/exercises', {
+     fastify.get('/exercises/:id', {
           preHandler: authenticate
      }, async (request, reply) => {
-          const exercises = await prisma.exercise.findMany({
+          const exercisesParams = z.object({
+               id: z.string().uuid()
+          });
+
+          const { id } = exercisesParams.parse(request.params);
+
+          const exercises = await prisma.exercise.findUnique({
+               where: {
+                    studentId: id
+               },
                select: {
                     active: true,
                     annotations: true,
@@ -30,5 +39,42 @@ export async function exercisesRoutes(fastify: FastifyInstance) {
           });
 
           return { exercises };
+     });
+
+     fastify.post('/exercises', {
+          preHandler: authenticate
+     }, async (request, reply) => {
+          const exercisesBody = z.object({
+               active: z.boolean(),
+               objective: z.string(),
+               endDate: z.string(),
+               startDate: z.string(),
+               studentId: z.string().uuid(),
+               list: z.string(),
+               type: z.string()
+          });
+
+          const {
+               active,
+               objective,
+               endDate,
+               startDate,
+               studentId
+          } = exercisesBody.parse(request.body);
+
+          await prisma.exercise.create({
+               data: {
+                    active,
+                    objective,
+                    endDate,
+                    startDate,
+                    studentId
+               }
+          });
+
+          return reply.status(201).send({
+               status: 'success',
+               message: 'Exercise created successfully.'
+          });
      });
 };
