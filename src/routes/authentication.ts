6f12e472b16e80d84e3b876e4b54d071a8faa178
Lucas Cristovam <cristhovamlucas@gmail.com>
2023-06-30 17:57:43 -0300

    FIX: fixing Schema and routes

--- src/routes/authentication.ts
+++ src/routes/authentication.ts
@@ -3,7 +3,68 @@ import { prisma } from '../lib/prisma';
 import { z } from 'zod';
 import bcrypt from 'bcrypt';
 
+const salt = 10;
+
 export async function authenticationRoutes(fastify: FastifyInstance) {
+     fastify.post('/students', async (request, reply) => {
+          try {
+               const studentSchema = z.object({
+                    name: z.string(),
+                    username: z.string(),
+                    email: z.string().email(),
+                    password: z.string(),
+                    birthdate: z.string(),
+                    telephone: z.string(),
+               });
+
+               const studentBody = studentSchema.parse(request.body);
+
+               const studentExists = await prisma.students.findFirst({
+                    where: {
+                         OR: [
+                              {
+                                   username: studentBody.username
+                              },
+                              {
+                                   email: studentBody.email
+                              },
+                         ],
+                    },
+               });
+
+               if (studentExists) return reply.status(400).send({
+                    status: 'error',
+                    message: 'Student already exists.'
+               });
+
+               const hashedPassword = await bcrypt.hash(studentBody.password, salt);
+
+               const student = await prisma.students.create({
+                    data: {
+                         name: studentBody.name,
+                         username: studentBody.username,
+                         email: studentBody.email,
+                         password: hashedPassword,
+                         birthdate: studentBody.birthdate,
+                         telephone: studentBody.telephone
+                    }
+               });
+
+               return reply.status(201).send({
+                    status: 'success',
+                    message: 'Aluno criado com sucesso',
+                    student
+               });
+          } catch (error) {
+               console.log(error);
+
+               return reply.status(500).send({
+                    status: 'error',
+                    message: `Ocorreu um erro: ${error}`
+               });
+          };
+     });
+
      fastify.post('/login/students', async (request, reply) => {
           const loginBody = z.object({
                email: z.string(),
@@ -12,7 +73,7 @@ export async function authenticationRoutes(fastify: FastifyInstance) {
 
           const { email, password } = loginBody.parse(request.body);
 
-          const studentExists = await prisma.student.findFirst({
+          const studentExists = await prisma.students.findFirst({
                where: {
                     email
                },
@@ -52,7 +113,7 @@ export async function authenticationRoutes(fastify: FastifyInstance) {
 
           const { email, password } = loginBody.parse(request.body);
 
-          const teacherExists = await prisma.teacher.findFirst({
+          const teacherExists = await prisma.teachers.findFirst({
                where: {
                     email
                },
